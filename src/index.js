433dbc342e88b673963c4092934505b0b54801aa
Serhii Solonko <sergeysolonko@gmail.com>
2016-07-15 21:44:56 +0200

    fixed typings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+///<reference path="typings/typings.d.ts"/>
+///<reference path="../typings/index.d.ts"/>
 "use strict";
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 ///<reference path="typings/typings.d.ts"/>
+///<reference path="../typings/index.d.ts"/>
+
 import { Express } from 'express';
 
 export * from './decorators';
