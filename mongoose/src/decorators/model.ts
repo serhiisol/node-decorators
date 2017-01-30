81442c1264657a8e1bd0d56b3be82fedff7279e2
Markus Fuchs <fuchs.markus@gmail.com>
2017-01-30 12:50:27 +0100

    feat(mongoose): Add SchemaTypeOpts as options parameter to Model decorator (#20)
    feat(mongoose): Add SchemaTypeOpts as options parameter to Model decorator (#20)

--- mongoose/src/decorators/model.ts
+++ mongoose/src/decorators/model.ts
@@ -1,11 +1,19 @@
 import { getMongooseMeta } from '../meta';
-
+import { SchemaTypeOpts } from 'mongoose';
 /**
  * Defines model class
  * @param {String} name Model name
+ * @param {SchemaTypeOpts} options
  */
-export let Model = (name: string) => {
+export let Model = (name: string, options?: SchemaTypeOpts<any>) => {
   return (target: Function) => {
-    getMongooseMeta(target.prototype).name = name;
+    const mongooseMeta = getMongooseMeta(target.prototype);
+    mongooseMeta.name = name;
+    if (options) {
+      Object.keys(options)
+        .forEach((key) => {
+          mongooseMeta.options.push(key, options[key]);
+        });
+    }
   }
 };
