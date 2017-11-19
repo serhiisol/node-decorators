31c48c446bdccef3cd6f5ca01728e5943a227b80
Serhii Solonko <sergeysolonko@gmail.com>
2017-11-19 13:33:36 +0100

    chore(mongoose): final adjustments

--- playground/mongoose/index.ts
+++ playground/mongoose/index.ts
@@ -1,15 +1,8 @@
 import { AnimalModel } from './model';
 
 AnimalModel.staticMethod();
-console.log(AnimalModel.staticField);
 
 let test = new AnimalModel({ testField: 'Hello World' });
-
-console.log('Model = ' + test.toString());
-
-// test.setField();
-
-// test.testField = 'Test Field';
 
 test.save(() => {
   console.log('saved 2');
