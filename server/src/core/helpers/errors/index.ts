ae3d49006d7bad1a8c2732a3e8d2a0ad7744b475
Just-Niko <125580651+Just-Niko@users.noreply.github.com>
2023-08-07 11:49:49 +0300

    fix(server): typo

--- server/src/core/helpers/errors/index.ts
+++ server/src/core/helpers/errors/index.ts
@@ -4,7 +4,7 @@ export { BadRequestError } from './bad-request';
 export { ForbiddenError } from './forbidden';
 export { InternalServerError } from './internal-server';
 export { NotAllowedError } from './not-allowed';
-export { NotFoundtError } from './not-found';
+export { NotFoundError } from './not-found';
 export { NotImplementedError } from './not-implemented';
 export { UnauthorizedError } from './unauthorized';
 export { UnavailableError } from './unavailable';
