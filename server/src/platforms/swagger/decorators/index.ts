c651fc3bc39e2eaa0146f1db51b496efb9839b81
Serhii Sol <sergeysolonko@gmail.com>
2023-08-10 00:07:48 +0300

    fix(server): swagger - fixed missing export (#189)

--- server/src/platforms/swagger/decorators/index.ts
+++ server/src/platforms/swagger/decorators/index.ts
@@ -1,4 +1,4 @@
 export { ApiOperation } from './api-operation';
 export { ApiParameter } from './api-parameter';
-export { ApiResponse } from './api-response';
+export * from './api-response';
 export * from './api-security';
